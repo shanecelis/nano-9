3c6cec16294a5d736aeed4ba54f98da8400a1af7
Shane Celis <shane.celis@gmail.com>
2024-08-14 17:15:45 -0400

    feature: Add loadimg.

--- assets/scripts/basic.lua
+++ assets/scripts/basic.lua
@@ -1,71 +1,74 @@
+-- local other = require("other")
+-- setpal(img)
 x = 0
 speed = 2
 animate = {
-    right = {
-        index = 8,
-        count = 4
-    },
-    scratch = {
-        index = 16,
-        count = 3
-    }
+  right = {
+    index = 8,
+    count = 4
+  },
+  scratch = {
+    index = 16,
+    count = 3
+  }
 }
-s = nil
 
+-- _init() is called once per load.
 function _init()
-    -- s = spr(8 * 24 + 15)
-    s = spr(8)
-    s.sx = 32 * 2
-    s.sy = 32 * 2
-    -- s.sy = 24
-    -- s.color = 15
-    -- s:drop()
-    -- s = nil
-    t = 0
-    cls(1)
+  img = loadimg("images/Goblin22.png")
+  setpal(img)
+  -- s = spr(8 * 24 + 15)
+  s = spr(8) -- load cat sprite
+  -- Make sprite two times bigger.
+  s.sx = 2 * 32
+  s.sy = 2 * 32
+  t = 0
+  cls(1)
 end
 
+-- _draw() is called once per frame, or 60 times a second.
 function _draw()
-    -- pset(x, x, 8)
-    -- pset(x, 127, 8)
-    x = x + 1
-    t = time() * speed * 4
-    -- t = t + 0.5
+  -- pset(x, y, c) sets the pixel at location (x, y) to color c.
+  -- pset(x, x, 8)
+  x = x + 1
+  -- t is our animation time.
+  t = time() * speed * 4
 
+  -- Check if 'z' is pressed.
+  if btnp(4) then
+    scratch_start = t           -- note cat scratch start time.
+  end
 
-    if btnp(4) then
-        scratch_start = t
-    end
+  if scratch_start then
+    -- cat is scratching
 
-    if scratch_start then
-        -- cat scratches
-        added = t - scratch_start
-        if added >= animate.scratch.count then
-            scratch_start = nil
-            s.index = 8
-        else
-            s.index = animate.scratch.index + added
-        end
-
+    -- scratch_frame is either 0, 1, 2 for the three frames.
+    scratch_frame = t - scratch_start
+    if scratch_frame >= animate.scratch.count then
+      scratch_start = nil       -- stop scratching
+      s.index = 8               -- set our animation to 8
     else
-        -- cat is not scratching
-        if btn(0) then
-            s.index = animate.right.index + t % animate.right.count
-            s.x = s.x - speed
-            s.flip_x = true
-        elseif btn(1) then
-            s.index = animate.right.index + t % animate.right.count
-            s.x = s.x + speed
-            s.flip_x = false
-        elseif btn(2) then
-            s.y = s.y - speed
-        elseif btn(3) then
-            s.y = s.y + speed
-        end
+      s.index = animate.scratch.index + scratch_frame
     end
-    -- s.x = x
-    -- s.y = x
-    -- s.index = x
-    --
-    --
+
+  else
+    -- cat is not scratching
+    if btn(0) then
+      -- go left
+      s.index = animate.right.index + t % animate.right.count
+      s.x = s.x - speed
+      s.flip_x = true
+    elseif btn(1) then
+      -- go right
+      s.index = animate.right.index + t % animate.right.count
+      s.x = s.x + speed
+      s.flip_x = false
+    elseif btn(2) then
+      -- go up
+      s.y = s.y - speed
+    elseif btn(3) then
+      -- go down
+      s.y = s.y + speed
+    end
+  end
 end
